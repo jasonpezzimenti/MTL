7c989e52f4a65af831d3c766362398e0552bec18
Jason Pezzimenti <96077246+jasonpezzimenti@users.noreply.github.com>
2022-06-23 12:07:05 +1000

    Created Token class.

---
+++ MTL/MTL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTL
+{
+	public class MTL
+	{
+		public class Token
+		{
+			public enum TokenTypes
+			{
+				Type = 0,
+				Identifier,
+				Value
+			}
+
+			public static TokenTypes TokenType { get; set; }
+		}
+
+		public static Stack<Token> Tokens = new Stack<Token>();
+	}
+}
