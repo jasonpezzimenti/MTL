478fed79e9be697796e5e67bb0857f59a9c8ec90
Jason Pezzimenti <96077246+jasonpezzimenti@users.noreply.github.com>
2022-06-23 12:14:07 +1000

    Created a Types SortedSet to store all MTL types.

---
+++ MTL/MTL.Types.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTL
+{
+	public partial class MTL
+	{
+		public static SortedSet<string> Types = new SortedSet<string>()
+		{
+			"container",
+			"text",
+			"list",
+			"image",
+			"link",
+			"separator"
+		};
+	}
+}
