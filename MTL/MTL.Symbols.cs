83260f076c151122e853a212e8e37da4c479e4f0
Jason Pezzimenti <96077246+jasonpezzimenti@users.noreply.github.com>
2022-06-23 12:16:45 +1000

    Created a Symbols List to match against symbols in user input.

---
+++ MTL/MTL.Symbols.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTL
+{
+	public partial class MTL
+	{
+		public static List<string> Symbols = new List<string>()
+		{
+			
+		};
+	}
+}
