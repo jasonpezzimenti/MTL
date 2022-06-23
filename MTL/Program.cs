a5b9cae208c0cae4d039e44dff1b6c9033d7b70b
Jason Pezzimenti <96077246+jasonpezzimenti@users.noreply.github.com>
2022-06-23 12:36:57 +1000

    Created fields inside Program class.

--- MTL/Program.cs
+++ MTL/Program.cs
@@ -2,6 +2,19 @@
 {
 	public class Program
 	{
+		public static string type = "",
+			identifier = "",
+			previous = "",
+			next = "",
+			character = "";
+		public static bool isInsideString,
+			isInsideScope,
+			isExpectingType,
+			isExpectingIdentifier,
+			isExpectingString,
+			isExpectingScopeStart,
+			isExpectingScopeEnd;
+
 		public static void Main(string[] arguments)
 		{
 
